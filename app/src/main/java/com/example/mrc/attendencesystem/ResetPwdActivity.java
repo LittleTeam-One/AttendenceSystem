3fe11e5799e7b4629b0d6896b88eb13f33bc5fc9
Mr.C <1510120023@vip.henu.edu.cn>
2018-04-03 18:14:51 +0800

    完成登录界面，忘记密码界面和重设密码界面

--- app/src/main/java/com/example/mrc/attendencesystem/ResetPwdActivity.java
+++ app/src/main/java/com/example/mrc/attendencesystem/ResetPwdActivity.java
@@ -1,16 +1,13 @@
 package com.example.mrc.attendencesystem;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ResetPwdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent loginIntent = new Intent(this ,LoginActivity.class);
-        startActivity(loginIntent);
+        setContentView(R.layout.activity_reset_pwd);
     }
 }
