f234aa2d0ce18be69c498dc2e4c317e77c70e3c3
Mr.C <1510120023@vip.henu.edu.cn>
2018-05-16 19:51:05 +0800

    修改页面

--- app/src/main/java/com/example/mrc/attendencesystem/activity/LocationActivity.java
+++ app/src/main/java/com/example/mrc/attendencesystem/activity/LocationActivity.java
@@ -3,30 +3,28 @@ package com.example.mrc.attendencesystem.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.baidu.mapapi.SDKInitializer;
-import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.MapView;
+
 import com.example.mrc.attendencesystem.R;
 
 public class LocationActivity extends AppCompatActivity {
-    MapView mMapView;
-    private BaiduMap mBaiduMap;
+    /*MapView mMapView;
+    private BaiduMap mBaiduMap;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SDKInitializer.initialize(getApplicationContext());
+     /*   SDKInitializer.initialize(getApplicationContext());*/
         setContentView(R.layout.activity_location);
-
+/*
         findView();
-        init();
+        init();*/
     }
 
-    void findView(){
+/*    void findView(){
         mMapView = (MapView)findViewById(R.id.map_view);
     }
 
     void init(){
         mBaiduMap = mMapView.getMap();
-    }
+    }*/
 }
