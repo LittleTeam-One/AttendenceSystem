43a13fdecf0a4927266b6489df76b2f4c7928694
C先生 <1439265824@qq.com>
2018-05-22 19:21:27 +0800

    添加聊天界面

---
+++ app/src/main/java/com/example/mrc/attendencesystem/activity/ChatActivity.java
@@ -0,0 +1,14 @@
+package com.example.mrc.attendencesystem.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mrc.attendencesystem.R;
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
